75ca2fa5536fe904793230f8a495136319537ccf
berger <robertberger.dev@gmail.com>
2023-02-06 16:54:19 -0500

    troubleshot and got the first part working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const name = React.createElement('h1', null, 'Robert Berger');
+const subTitle = React.createElement('p', null, 'Aspiring Software Developer');
+const header = React.createElement('div', null, [name, subTitle]);
+
+const container = React.createElement('div', null, [header]);
+
+ReactDOM.render(
+  container,
+  document.getElementById('root')
+);
