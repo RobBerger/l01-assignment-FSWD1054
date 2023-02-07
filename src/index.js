aa3be746e6ac9617915a8c99a53127ed218cafd1
berger <robertberger.dev@gmail.com>
2023-02-07 13:52:28 -0500

    trying to get list items to render

--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ const name = React.createElement('h1', null, 'Robert Berger');
 const subTitle = React.createElement('p', null, 'Aspiring Software Developer');
 const container1 = React.createElement('div',{id:'header'}, [name, subTitle]);
 
-const aboutMe = ['Focused', 'Avid-Reader', 'Pilot'];
+let aboutMe = ['Focused', 'Avid-Reader', 'Pilot'];
+
+// aboutMe.map((value) => React.createElement('li', null, {value}));
+
+aboutMe.map(function (value) {
+  return React.createElement('li', null, `${value}`);
+});
+
+const container2 = React.createElement('ul', null, [aboutMe]);
 
 
-
-const container = React.createElement('div', null, [container1]);
+const container = React.createElement('div', null, [container1, container2]);
 
 ReactDOM.render(
   container,
